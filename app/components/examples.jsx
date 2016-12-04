6d409936674fa69ceb787d195ba64ccfa986f065
Jorge Andrade <jacandrade@gmail.com>
2016-12-04 14:31:07 -0200

    Examples page styled

--- app/components/examples.jsx
+++ app/components/examples.jsx
@@ -1,9 +1,14 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples =(props) => {
     return (<div>
-        <h3>Examples</h3>
-        <p>Welcome to examples page!</p>
+        <h1 className="text-centered">Examples</h1>
+        <p>Here are a few example locations to try out:</p>
+        <ol>
+            <li><Link to='/?location=Philadelphia'>Philadelphia, PA</Link></li>
+            <li><Link to='/?location=Rio,Br'>Rio de Janeiro, BR</Link></li>
+        </ol>
     </div>);
 };
 
